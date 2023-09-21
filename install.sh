b4a1634197d044e574c49ae469a85de42401682f
mine-Proxy <97606543+mine-Proxy@users.noreply.github.com>
2023-09-22 00:39:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 VERSION="3.6.1"
 DOWNLOAD_HOST="https://github.com/mine-Proxy/TcstMinerSystem/raw/main/linux"
-ORIGIN_EXEC="rustminersystem-${VERSION}"
+ORIGIN_EXEC="tcstminersystem-${VERSION}"
 
 SERVICE_NAME="rustservice"
 
